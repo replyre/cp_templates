2dae50f7252915555f5ecf42a2c391e674a74117
Frankenstein <121796450+replyre@users.noreply.github.com>
2024-12-22 11:29:47 +0530

    Update Minimum_spanning_tree.cpp

--- Minimum_spanning_tree.cpp
+++ Minimum_spanning_tree.cpp
@@ -6,7 +6,7 @@
 
 // Minimum sum of all the weights of edges is MST- Minimum Spanning Tree
 
-// Prims Algo for MST
+// ------------------------------------------------------------------Prims Algo for MST--------------------------------------------------------------------------
 // base:
 // Priority queue
 // Visisted Array [wt, node, parent]
@@ -73,3 +73,113 @@ int spanningTree(int V, vector<vector<int>> adj[])
 
     return sum; // Return the total weight of the MST
 }
+
+// ------------------------------------------------Kruskal's Algo--------------------------------------------------------
+// sort all the edges to weight
+// Do DSU, if parent same means already added, no need to consider
+// otherwise add it
+
+class DisjointSet
+{
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+    int findParent(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+
+        int ult_par = findParent(u);
+        int vlt_par = findParent(v);
+
+        if (ult_par == vlt_par)
+            return;
+        if (rank[ult_par] < rank[vlt_par])
+        {
+            parent[ult_par] = vlt_par;
+        }
+        else if (rank[vlt_par] < rank[ult_par])
+            parent[vlt_par] = ult_par;
+        else
+        {
+            parent[vlt_par] = ult_par;
+            rank[vlt_par]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+
+        int ult_par = findParent(u);
+        int vlt_par = findParent(v);
+
+        if (ult_par == vlt_par)
+            return;
+        if (size[ult_par] < size[vlt_par])
+        {
+            parent[ult_par] = vlt_par;
+            size[vlt_par] += size[ult_par];
+        }
+        else
+        {
+            parent[vlt_par] = ult_par;
+            size[ult_par] += size[vlt_par];
+        }
+    }
+    void display_Size()
+    {
+        for (int i = 0; i < size.size(); i++)
+            cout << size[i];
+    }
+};
+
+class Solution
+{
+public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+
+        vector<pair<int, pair<int, int>>> vt;
+        for (int i = 0; i < V; i++)
+        {
+            for (auto &a : adj[i])
+            {
+                vt.push_back({a[1], {i, a[0]}});
+            }
+        }
+        sort(vt.begin(), vt.end());
+        DisjointSet DS(vt.size());
+        int ans = 0;
+        for (int i = 0; i < vt.size(); i++)
+        {
+            if (DS.findParent(vt[i].second.first) == DS.findParent(vt[i].second.second))
+            {
+                continue;
+            }
+            DS.unionByRank(vt[i].second.first, vt[i].second.second);
+            ans += vt[i].first;
+
+            // cout<<" "<<vt[i].second.first<<" "<<vt[i].second.second<<" "<<vt[i].first<<endl;
+        }
+
+        return ans;
+    }
+};

e55a5bfe88967576d1ba11345a9253731bb94795
Frankenstein <121796450+replyre@users.noreply.github.com>
2024-12-24 11:57:59 +0530

    Update binary_search_problems.cpp

--- faltu_mistakes_herustics/binary_search_problems.cpp
+++ faltu_mistakes_herustics/binary_search_problems.cpp
@@ -28,3 +28,27 @@ int findMin(vector<int> &arr)
     }
     return arr[l];
 }
+
+
+// search in 2d sorted matrix
+// the ros are multiple of (m-1) the size of row;
+//the cols are modulo of m-1; as we keep moving over them;
+//example: 
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        int n= mat.size();
+        int m= mat[0].size();
+        int l=0;
+        int h=n*m-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            int r= mid/m, col=mid%m;
+            if(mat[r][col]==x) return true;
+            if(mat[r][col]< x) l=mid+1;
+            else h=mid-1;
+        }
+        return false;
+    }
+};
